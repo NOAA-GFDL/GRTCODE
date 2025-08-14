9fbab7f8b97fc28745d9b22e3da7804a87c66a5f
JingFeng <41129443+fengzydy@users.noreply.github.com>
2025-08-14 15:23:26 -0400

    Update run-era5.sh

--- GRTworkflow/run-era5.sh
+++ GRTworkflow/run-era5.sh
@@ -81,7 +81,7 @@ while [[ $# -gt 0 ]]; do
     ;;
   esac
 done
-if [ $counter -ne 1 ]; then
+if [ $counter -lt 1 ]; then
   echo "${counter}"
   echo "Error: missing required argument."
   echo "Usage: $argument_list"
